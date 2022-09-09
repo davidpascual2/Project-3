143bf8b80b996d33e033fdfa1a8973a8ee1b7e07
David Pascual <David@Dairas-MacBook-Air.local>
2022-09-09 12:28:51 -0700

    added user-routes

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -2,12 +2,22 @@ const router = require('express').Router();
 const {
     createUser,
     getSingleUser,
+    saveProperty,
+    deleteProperty,
     // getUsers?,
     login
 } = require('../../controllers/user-controller')
 
 // add middleware if needed
+const { authMiddleware } = require('../../utils/auth');
 
 // add authMiddleware if needed
+router.route('/').post(createUser).put(authMiddleware, saveProperty);
+
+router.route('/login').post(login);
+
+router.route('/me').get(authMiddleware, getSingleUser);
+
+router.route('/properties/propertyId').delete(authMiddleware, deleteProperty);
 
 module.exports = router;
