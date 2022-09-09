d18707ea75a53a9a6912317cdbc550d02aadf76a
David Pascual <David@Dairas-MacBook-Air.local>
2022-09-08 20:59:31 -0700

    commented out prop mod

--- server/models/Property.js
+++ server/models/Property.js
@@ -2,33 +2,35 @@ const { Schema } = require('mongoose');
 
 const propertySchema = new Schema({
 
-    city: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    price: {
-        type: String, //??????
+//CHANGE AS NEEDED!!!
 
-    },
-    propertyId: {
-        type: String,
-    },
-    bed: {
-        type: String
-    },
-    bath: {
-        type: String
-    },
-    lot: {
-        type: String
-    },
-    sqft: { //size?
-        type: String
-    },
-    year: {
-        type: String
-    }
+//     city: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//     },
+//     price: {
+//         type: String, //??????
+
+//     },
+//     propertyId: {
+//         type: String,
+//     },
+//     bed: {
+//         type: String
+//     },
+//     bath: {
+//         type: String
+//     },
+//     lot: {
+//         type: String
+//     },
+//     sqft: { //size?
+//         type: String
+//     },
+//     year: {
+//         type: String
+//     }
 
 });
 
