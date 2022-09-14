3bd1099f2ea69e30e51ade55b6ee2d004ca4a356
David Pascual <David@Dairas-MacBook-Air.local>
2022-09-14 09:15:51 -0700

    fixed index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,3 @@
 const User = require('./User')
-const Property = require('./Property') // should this be a model?
 //ADD ANY ADDITIONAL MODEL PAGES
-module.exports = { User, Property };
+module.exports = { User };
