60583d188496945bcab8ae7a014f57a5a1646162
David Pascual <David@Dairas-MacBook-Air.local>
2022-09-15 19:46:41 -0700

    fixed saved prop

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,9 +5,10 @@ const { AuthenticationError } = require('apollo-server-express');
 const resolvers = {
     Query: {
         me: async(parent, args, context) => {
+            console.log(context.user, "$$$$$$$$$$$", args)
             if (context.user) {
                 const userData = await User.findOne({_id: context.user._id})
-                .select('-__v -password')
+                // .select('-__v -password')
                 .populate('savedProperties');
 
                 return userData;
