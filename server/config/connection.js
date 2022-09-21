fae1408c00cd8c27732996fb96f0c45511808c68
davidpascual2 <35315292+davidpascual2@users.noreply.github.com>
2022-09-20 18:30:35 -0700

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:/test', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:/propertyDB', {
     userNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
